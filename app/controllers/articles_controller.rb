cfda5fc88f2234057ac7e7df05c0b79ae1943616
Renan <renanthompsom@gmail.com>
2024-03-19 12:16:13 -0300

    Add show action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,9 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def show
+    @article = Article.find_by(id: params[:id])
+    redirect_to articles_path if @article.blank?
+  end
 end
